939ed5d43943774732e9ffda866d1d056ec64907
Roman <62894917+Wolfengo@users.noreply.github.com>
2026-01-04 14:11:26 +0300

    Update hyprland.nix

--- .config/nixos-manager/modules/desktop/desktop/hyprland/hyprland.nix
+++ .config/nixos-manager/modules/desktop/desktop/hyprland/hyprland.nix
@@ -1,8 +1,77 @@
 { config, pkgs, ... }:
+
 {
-	programs.hyprland = {
-		enable = true;
-		withUWSM = true;
-		xwayland.enable = true;
-	};
+  ########################
+  # Hyprland
+  ########################
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
+
+  ########################
+  # Wayland essentials
+  ########################
+  environment.systemPackages = with pkgs; [
+    wayland
+    wayland-utils
+
+    # XWayland
+    xwayland
+
+    # Clipboard
+    wl-clipboard
+    cliphist
+
+    # Screenshots
+    grim
+    slurp
+
+    # Cursor / utils
+    seatd
+    libinput
+
+    # Notifications
+    mako
+
+    # App launcher
+    rofi-wayland
+
+    # Status bar
+    waybar
+
+    # Portals
+    xdg-desktop-portal
+    xdg-desktop-portal-hyprland
+
+    # Polkit agent
+    polkit_gnome
+
+    # Brightness & audio
+    brightnessctl
+    pamixer
+  ];
+
+  ########################
+  # Services
+  ########################
+  services.dbus.enable = true;
+
+  services.seatd.enable = true;
+
+  ########################
+  # XDG Portals (КРИТИЧНО)
+  ########################
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+    ];
+  };
+
+  ########################
+  # Security / Polkit
+  ########################
+  security.polkit.enable = true;
 }
