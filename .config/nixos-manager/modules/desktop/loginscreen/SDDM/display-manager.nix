d13eb315b5e01755fd1161393626f21446df8bae
Roman <62894917+Wolfengo@users.noreply.github.com>
2026-01-04 14:12:38 +0300

    Update display-manager.nix

--- .config/nixos-manager/modules/desktop/loginscreen/SDDM/display-manager.nix
+++ .config/nixos-manager/modules/desktop/loginscreen/SDDM/display-manager.nix
@@ -1,9 +1,13 @@
 { config, pkgs, ... }:
-{
-	services.xserver.enable = true;
 
-	services.displayManager = {
-		sddm.enable = true;
-		sddm.wayland.enable = true;
-	};
+{
+  services.xserver.enable = true;
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+
+  # Для Wayland-сессий
+  services.displayManager.defaultSession = "hyprland";
 }
